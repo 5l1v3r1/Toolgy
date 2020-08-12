83b4820867974238a930ea00114d3469cb20a0da
wnagzihxa1n <wnagzihxa1n@gmail.com>
2020-08-12 10:47:21 +0800

    Rewrite get_top_activity

---
+++ tools/general_tool.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from global_config.select_item import SelectItem
+from tools.device_tool import DeviceTool
+from tools.shell_tool import ShellTool
+
+
+class GeneralTool:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_top_activity(buttons_panel):
+        device_build_version = DeviceTool.getprop_ro_build_version_release()
+        shell = ''
+        if device_build_version[0].startswith("7"):
+            shell = 'adb -s {} shell dumpsys activity | grep "mFocusedActivity"' \
+                .format(SelectItem.get_selected_device_name())
+        elif device_build_version[0].startswith("8"):
+            shell = 'adb -s {} shell dumpsys activity activities | grep "mResumedActivity"' \
+                .format(SelectItem.get_selected_device_name())
+        buttons_panel.textctrl_shell.SetValue(shell)
+        out, err = ShellTool.run(shell)
+        buttons_panel.textctrl_output.SetValue(out)
+        buttons_panel.textctrl_output.AppendText('\n')
+        buttons_panel.textctrl_output.AppendText(err)
