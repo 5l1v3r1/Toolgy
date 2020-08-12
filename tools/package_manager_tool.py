c85f67053b5bfc00a3cd66075d864e24b923ae46
wnagzihxa1n <wnagzihxa1n@gmail.com>
2020-08-12 10:47:41 +0800

    Add install_app

--- tools/package_manager_tool.py
+++ tools/package_manager_tool.py
@@ -1,4 +1,8 @@
 # coding=utf-8
+import os
+
+import wx
+
 
 class PackageManagerTool:
     def __init__(self):
@@ -9,8 +13,14 @@ class PackageManagerTool:
         pass
 
     @staticmethod
-    def install_app():
-        pass
+    def install_app(buttons_panel):
+        dir_name = ''
+        dialog = wx.FileDialog(buttons_panel, 'Choose a apk file', dir_name, "", "*.*", wx.FD_OPEN)
+        if dialog.ShowModal() == wx.ID_OK:
+            file_name = dialog.GetFilename()
+            dir_name = dialog.GetDirectory()
+            file_path = os.path.join(dir_name, file_name)
+            buttons_panel.textctrl_shell.SetValue('adb install {}'.format(file_path.replace(' ', '\ ')))
 
     @staticmethod
     def uninstall_app(buttons_panel, package_name):
